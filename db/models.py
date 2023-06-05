ce705a2a90e041c19b19dffd98840d0b349ff64b
Mihail <vertigomisha@gmail.com>
2023-06-05 23:20:59 +0500

    first commit

---
+++ db/models.py
@@ -0,0 +1,63 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import create_engine, Column, Integer, SmallInteger, String, DateTime, Boolean, ForeignKey, BigInteger
+
+_BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_path = os.path.join(_BASE_DIR, '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+_db_host = os.environ['POSTGRES_HOST']
+_db_username = os.environ['POSTGRES_USERNAME']
+_db_password = os.environ['POSTGRES_PASSWORD']
+_db_name = os.environ['POSTGRES_DB']
+engine = create_engine(
+    f'postgresql://{_db_username}:{_db_password}@{_db_host}:5432/{_db_name}', echo=True)
+
+Base = declarative_base()
+Session = sessionmaker()
+
+
+class Group(Base):
+    __tablename__ = 'group'
+
+    id = Column(SmallInteger, primary_key=True)
+    tg_id = Column(BigInteger, nullable=False)  # tg group id
+    name = Column(String(50), nullable=False)
+    calendar = relationship("Calendar", back_populates="group", uselist=False)
+
+    def __repr__(self):
+        return f'<Telegram group - {self.name}, id: {self.id}>'
+
+
+class Calendar(Base):
+    __tablename__ = 'calendar'
+
+    id = Column(SmallInteger, primary_key=True)
+    name = Column(String(50), nullable=False)
+    group_id = Column(Integer, ForeignKey("group.id"))
+    group = relationship("Group", back_populates="calendar")
+    event = relationship("Event", back_populates="calendar")
+
+    def __repr__(self):
+        return f'<Calendar - name: {self.name}, group id: {self.group_id}>'
+
+
+class Event(Base):
+    __tablename__ = 'events'
+
+    id = Column(SmallInteger, primary_key=True)
+    start = Column(DateTime, nullable=False)
+    end = Column(DateTime, nullable=False)
+    description = Column(String(255), nullable=False)
+    calendar_id = Column(Integer, ForeignKey("calendar.id"))
+    calendar = relationship("Calendar", back_populates="event")
+    author_id = Column(Integer, nullable=False)
+    author_firstname = Column(String(255), nullable=False)
+    author_username = Column(String(255), nullable=True)
+    is_archive = Column(Boolean, default=False, nullable=True)
+
+    def __repr__(self):
+        return f'<Event - start: {self.start}, end: {self.end}>'
---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+
+from telegram.ext import Updater
+
+from handlers import start, reserve, initiate, display, my_events, feedback, help, repair
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+token = os.environ['BOT_TOKEN']
+
+if __name__ == '__main__':
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(start.start_handler)
+    dispatcher.add_handler(repair.repair_handler)
+    dispatcher.add_handler(reserve.reserve_handler)
+    dispatcher.add_handler(initiate.initiate_handler)
+    dispatcher.add_handler(display.display_handler)
+    dispatcher.add_handler(display.option_handler)
+    dispatcher.add_handler(my_events.my_events_handler)
+    dispatcher.add_handler(my_events.event_handler)
+    dispatcher.add_handler(my_events.del_handler)
+    dispatcher.add_handler(my_events.edit_handler)
+    dispatcher.add_handler(feedback.feedback_handler)
+    dispatcher.add_handler(help.help_handler)
+
+    # dispatcher.add_error_handler(error.UnauthorizedError)
+
+    updater.start_polling()
