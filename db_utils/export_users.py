48895de4904c3b7a6689937fce4aad15ecdf76cc
czar_code <vertigomisha@gmail.com>
2023-06-07 13:05:31 +0500

    error exporting users

--- db_utils/export_users.py
+++ db_utils/export_users.py
@@ -5,18 +5,20 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 from db.models import User, Session, engine
+from sqlalchemy import select
 
 df = pd.read_excel('flatfiles/test_users.xlsx')
 if not df.empty:
     session = Session(bind=engine)
     
     for index, row in df.iterrows():
-        tg_id = row['Telegram Id']
+        tg_id = int(row['Telegram Id'])
         name = row['Name']
-        print('!!!!!!!!!!!!!!!!!!!!!!'*3, tg_id, name)
-    
-        user_exists = session.query(User).filter(User.tg_id == tg_id).first() is not None
-        if user_exists:
+
+        stmt = select(User).where(User.tg_id == int(tg_id))
+        result = session.execute(stmt).scalars().all()
+        print('1231212312312312321312312', result)
+        if result:
             print(f'{name} {tg_id} уже существует')
         else:
             user = User(tg_id=tg_id, name=name)
