be013441eff3591c72f9669bb84be99aaed73788
czarmisha <vertigomisha@gmail.com>
2023-06-14 23:43:56 +0500

    asd

--- handlers/utils/analyze.py
+++ handlers/utils/analyze.py
@@ -1,5 +1,7 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
+from sqlalchemy import select
+from db.models import Keyword, Topic
 
 # Исходный текст вопроса и список ключевых слов
 question = "Ваш вопрос"
@@ -24,32 +26,46 @@ for keyword in lemmatized_keywords:
 # Выводим найденные ключевые слова
 print(matched_keywords)
 class AnalyzeQuestion():
-    def __init__(self, question) -> None:
+    def __init__(self, question, session) -> None:
+        self.session = session
         self.question = question
         self.keywords = []
         self.set_keyword()
 
     def set_keywords(self):
-        pass
+        keyword = []
+        stmt = select(Keyword)
+        result = self.session.execute(stmt).scalars().all()
+        print('!!!!!!!!!!!!!!!', 'set_keywords')
+        for keyword in result:
+            print('@', keyword.value)
+            keywords.append(keyword)
+
+        self.keywords = list(set(keywords))
 
     def do_analyze(self):
         # Создаем экземпляр лемматизатора
         lemmatizer = WordNetLemmatizer()
-
         # Лемматизируем ключевые слова
         lemmatized_keywords = [lemmatizer.lemmatize(keyword.lower()) for keyword in self.keywords]
-
         # Преобразуем текст вопроса в список отдельных слов
         words = nltk.word_tokenize(self.question.lower())
-
         # Ищем совпадения ключевых слов в тексте вопроса
         matched_keywords = []
         for keyword in lemmatized_keywords:
             keyword_words = nltk.word_tokenize(keyword)
             if all(word in words for word in keyword_words):
                 matched_keywords.append(keyword)
-
-        return self.get_themes()
+        
+        self.matched_keywords = matched_keywords
+        return self.get_topics()
     
-    def get_themes(self):
-        pass
+    def get_topics(self):
+        stmt = select(Keyword).where(Keyword.value.in_(self.matched_keywords))
+        result = self.session.execute(stmt).scalars().all()
+        ids = [keyword.topic_id for keyword in result]
+
+        stmt = select(Topic).where(Topic.id.in_(ids))
+        result = self.session.execute(stmt).scalars().all()
+        self.topics = {topic.name: topic.user_id for topic in result}
+        return self.topics
