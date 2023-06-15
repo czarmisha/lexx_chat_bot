216e51c08bf41b32b12d7ebed72c2390548e5b59
czar_code <vertigomisha@gmail.com>
2023-06-15 10:08:45 +0500

    process

--- utils/analyze.py
+++ utils/analyze.py
@@ -1,11 +1,13 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
+# import nltk
+# from nltk.stem import WordNetLemmatizer
+import spacy
 from sqlalchemy import select
 from db.models import Keyword, Topic
 
 
 class AnalyzeQuestion():
     def __init__(self, session) -> None:
+        self.topics = None
         self.session = session
         self.keywords = []
         self.set_keywords()
@@ -19,26 +21,36 @@ class AnalyzeQuestion():
         result = self.session.execute(stmt).scalars().all()
         print('!!!!!!!!!!!!!!!', 'set_keywords')
         for keyword in result:
-            print('@', keyword.value)
             keywords.append(keyword.value)
 
         self.keywords = list(set(keywords))
+        print('!!!!@@@@', self.keywords)
 
     def do_analyze(self):
-        # Создаем экземпляр лемматизатора
-        lemmatizer = WordNetLemmatizer()
-        # Лемматизируем ключевые слова
-        lemmatized_keywords = [lemmatizer.lemmatize(keyword.lower()) for keyword in self.keywords]
-        # Преобразуем текст вопроса в список отдельных слов
-        words = nltk.word_tokenize(self.question.lower())
-        # Ищем совпадения ключевых слов в тексте вопроса
+        # # Создаем экземпляр лемматизатора
+        # lemmatizer = WordNetLemmatizer()
+        # # Лемматизируем ключевые слова
+        # lemmatized_keywords = [lemmatizer.lemmatize(keyword.lower()) for keyword in self.keywords]
+        # # Преобразуем текст вопроса в список отдельных слов
+        # words = nltk.word_tokenize(self.question.lower())
+        # # Ищем совпадения ключевых слов в тексте вопроса
+        # matched_keywords = []
+        # for keyword in lemmatized_keywords:
+        #     keyword_words = nltk.word_tokenize(keyword)
+        #     if all(word in words for word in keyword_words):
+        #         matched_keywords.append(keyword)
+        nlp = spacy.load("ru_core_news_sm")  # Загрузка предварительно обученной модели языка
+        question_doc = nlp(self.question)
+
         matched_keywords = []
-        for keyword in lemmatized_keywords:
-            keyword_words = nltk.word_tokenize(keyword)
-            if all(word in words for word in keyword_words):
+        for keyword in self.keywords:
+            keyword_doc = nlp(keyword)
+
+            if any(keyword_token.text.lower() in question_token.text.lower() for question_token in question_doc for keyword_token in keyword_doc):
                 matched_keywords.append(keyword)
-        
+
         self.matched_keywords = matched_keywords
+        print('!!!!!!!!!!!!!!!matched_keywords', matched_keywords)
         return self.get_topics()
     
     def get_topics(self):
