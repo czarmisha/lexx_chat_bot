be8e707716359ea4e27fe28fde8ee1ee94b9718b
LexxITstorage <admin@lexxinfo.com>
2023-07-24 18:18:15 +0500

    Add github workflow

---
+++ services/post_upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if systemctl stop chat_bot.service ; then
+    sudo cp /home/ubuntu/chat_bot/services/chat_bot.service /etc/systemd/system/
+else
+    echo "No chat_bot.service found in system, now creating"
+    sudo cp /home/ubuntu/chat_bot/services/chat_bot.service /etc/systemd/system
+fi
+sudo systemctl daemon-reload
+sudo systemctl enable chat_bot.service
+sudo systemctl start chat_bot.service
+rm  /home/ubuntu/chat_bot.tar
