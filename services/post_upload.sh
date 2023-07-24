4128ec1c19ac17df3c7c648c8450e4307a7c430c
LexxITstorage <admin@lexxinfo.com>
2023-07-24 18:20:28 +0500

    Bug fix

--- services/post_upload.sh
+++ services/post_upload.sh
@@ -2,7 +2,7 @@
 if systemctl stop chat_bot.service ; then
     sudo cp /home/ubuntu/chat_bot/services/chat_bot.service /etc/systemd/system/
 else
-    echo "No chat_bot.service found in system, now creating"
+    echo "No chat_bot.service found in system, now will creating"
     sudo cp /home/ubuntu/chat_bot/services/chat_bot.service /etc/systemd/system
 fi
 sudo systemctl daemon-reload
